a3f627499cf05f9bdf72f92a27387ae98bff02c2
sitibudi <85599618+sitibudi@users.noreply.github.com>
2023-03-20 20:58:08 +0700

    3 sensor
    configure 3 sensor into PICO D4

--- TugasAkhirHMR.ino
+++ TugasAkhirHMR.ino
@@ -1,32 +1,37 @@
 //=====================================================LIBRARY=======================================================================
-#include "Wire.h"
-#include <MPU6050_light.h>
-
+#include <DFRobot_MLX90614.h>
+#include <DFRobot_MAX30102.h>
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
 //======================================================DONE=========================================================================
 
 
 //==================================================INITIALIZATION=======================================================================
-MPU6050 mpu(Wire);
-unsigned long timer = 0;
+// define object (instantiate an object to drive our sensor)
+// Temp Sensor
+DFRobot_MLX90614_I2C sensor;   // 
+
+// Heart Beat Sensor
+DFRobot_MAX30102 particleSensor; 
+
+// IMU sensor
+Adafruit_MPU6050 mpu;
+sensors_event_t a, g, temp;
+
+
+// millis 
+int current, pre_current;
+int current1, pre_current1;
+
+
+// Global variable
+
 
 //======================================================DONE=========================================================================
 
 //====================================================FUNCTION=======================================================================
 
-// MPU 6050 SETUP
-void 6050Setup(){
-   byte status = mpu.begin();
-  Serial.print(F("MPU6050 status: "));
-  Serial.println(status);
-  while(status!=0){ } // stop everything if could not connect to MPU6050
-  
-  Serial.println(F("Calculating offsets, do not move MPU6050"));
-  delay(1000);
-  // mpu.upsideDownMounting = true; // uncomment this line if the MPU6050 is mounted upside-down
-  mpu.calcOffsets(); // gyro and accelero
-  Serial.println("Done!\n");
-  
-}
 
 //======================================================DONE=========================================================================
 
@@ -35,15 +40,49 @@ void 6050Setup(){
 // ===                      INITIAL SETUP                       ===
 // ================================================================
 void setup() {
- Serial.begin(9600);
-  Wire.begin();
-//  pinMode();
-//  pinMode();
-//  pinMode();
-//  pinMode();
-  6050Setup()
- 
+ Serial.begin(115200);
 
+  // initialize the sensor
+  while( !particleSensor.begin() and !sensor.begin() ){
+    Serial.println("Communication with device failed, please check connection");
+    delay(3000);
+  }
+  Serial.println("Begin ok!");
+
+  // Try to initialize!
+  if (!mpu.begin()) {
+    Serial.println("Failed to find MPU6050 chip");
+    while (1) {
+      delay(10);
+    }
+  }
+
+   // RANGE 
+//  mpu.setAccelerometerRange(MPU6050_RANGE_2_G);
+//  mpu.setAccelerometerRange(MPU6050_RANGE_4_G);
+//  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+  mpu.setAccelerometerRange(MPU6050_RANGE_16_G);
+
+  // DEGREE
+//  mpu.setGyroRange(MPU6050_RANGE_250_DEG);
+//  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+//  mpu.setGyroRange(MPU6050_RANGE_1000_DEG);
+  mpu.setGyroRange(MPU6050_RANGE_2000_DEG);
+
+  // BANDWIDTH
+//  mpu.setFilterBandwidth(MPU6050_BAND_5_HZ);
+//  mpu.setFilterBandwidth(MPU6050_BAND_10_HZ);
+//  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+//  mpu.setFilterBandwidth(MPU6050_BAND_44_HZ);
+//  mpu.setFilterBandwidth(MPU6050_BAND_94_HZ);
+//  mpu.setFilterBandwidth(MPU6050_BAND_184_HZ);
+  mpu.setFilterBandwidth(MPU6050_BAND_260_HZ);
+  Serial.println("");
+
+  particleSensor.sensorConfiguration(/*ledBrightness=*/60, /*sampleAverage=*/SAMPLEAVG_8, \
+                                  /*ledMode=*/MODE_MULTILED, /*sampleRate=*/SAMPLERATE_400, \
+                                  /*pulseWidth=*/PULSEWIDTH_411, /*adcRange=*/ADCRANGE_16384);
+  
 }
 
 
@@ -52,21 +91,47 @@ void setup() {
 // ================================================================
 
 void loop() {
-    mpu.update();
+   // MLX906 
+current = millis();
+current1 = millis();
+
+  if(current - pre_current > 50){
+    Temp = sensor.getObjectTempCelsius();
+    mpu.getEvent(&a, &g, &temp);
+  //    Serial.print("AccelX:");
+  // Serial.print(a.acceleration.x);
+  // Serial.print(",");
+  // Serial.print("AccelY:");
+  // Serial.print(a.acceleration.y);
+  // Serial.print(",");
+  // Serial.print("AccelZ:");
+  // Serial.print(a.acceleration.z);
+  // Serial.print(", ");
+  // Serial.print("GyroX:");
+  // Serial.print(g.gyro.x);
+  // Serial.print(",");
+  // Serial.print("GyroY:");
+  // Serial.print(g.gyro.y);
+  // Serial.print(",");
+  // Serial.print("GyroZ:");
+  // Serial.print(g.gyro.z);
+  // Serial.println("");
     
-    if((millis()-timer)>10){ // print data every 10ms
-  Serial.print("X : ");
-  Serial.print(mpu.getAngleX());
-  Serial.print("\tY : ");
-  Serial.print(mpu.getAngleY());
-  Serial.print("\tZ : ");
-  Serial.println(mpu.getAngleZ());
-  
-  timer = millis();  
+     
+  Serial.print("temp : ");  Serial.print(objectTemp);  Serial.print("|| ");
+  Serial.print("beat:"); Serial.print(particleSensor.getIR());  Serial.print("|| ");
+  Serial.print("AccelX:"); Serial.print(a.acceleration.x);Serial.print("|");
+  Serial.print("AccelY:"); Serial.print(a.acceleration.y);Serial.print("|");
+  Serial.print("AccelZ:"); Serial.print(a.acceleration.z);Serial.println("|");
+  pre_current = current;
   }
- if (mpu.getAngleY() > 70){
-    Serial.println("get 70 angle");
-    delay(1000);
-  }
+
+  // if (current1 - pre_current1 > 50){
+  //   pre_current1 = current1;
+   
+
+  // }
+
+ 
 
 }
